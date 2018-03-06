d6ebf56cc6de2cc4ba7a870bd50e5ee392d414d1
Heidi Mason <heidi@ipcprint.com>
2018-03-05 23:23:38 -0800

    feat: Add search + sort functionality

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class SearchBooks extends Component {
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+        this.setState({
+            query: query
+        })
+    }
+
 	render() {
-		const { books } = this.props
+		const { books } = this.props,
+			  { query } = this.state
+
+		let showingBooks
+
+		books.sort(sortBy('title'))
+
+		if (query) {
+            // escapeRegExp = escape special characters
+            // i = ignore case i.e. Not case sensitive
+            const match = new RegExp(escapeRegExp (query), 'i');
+
+            showingBooks = books.filter( (book) => match.test(book.title) || match.test(book.author) );
+        } else {
+            showingBooks = books;
+        }
 
 		return (
 			<div className="search-books">
             	<div className="search-books-bar">
-              		<Link className="close-search" to="/">Close</Link>
+              		<Link to="/"
+              			className="close-search">Close
+              		</Link>
 
               		<div className="search-books-input-wrapper">
 		                {/*
@@ -20,13 +49,17 @@ class SearchBooks extends Component {
 		                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
 		                  you don't find a specific author or title. Every search is limited by search terms.
 		                */}
-                		<input type="text" placeholder="Search by title or author"/>
+                		<input type="text"
+                			placeholder="Search by title or author"
+                			value={query}
+                			onChange={(event) => this.updateQuery(event.target.value)}
+                		/>
               		</div>
             	</div>
 
 	            <div className="search-books-results">
 	            	<ListBooks
-	            		showingBooks={books}
+	            		showingBooks={showingBooks}
 	            	/>
 	            </div>
           	</div>
