9009680fe76626e64bf865f9382b88ed16c1fc1b
Heidi Mason <hapaheidi@gmail.com>
2019-01-08 14:03:53 -0800

    test: Add to SearchBooks component

--- src/components/SearchBooks.test.js
+++ src/components/SearchBooks.test.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import ListShelves from './ListShelves'
+import SearchBooks from './SearchBooks'
 import { BrowserRouter, Link } from 'react-router-dom'
 
-describe('<ListShelves />', () => {
-	it('link to search page renders correctly', () => {
+describe('<SearchBooks />', () => {
+	it('link to home page renders correctly', () => {
 		const tree = renderer
 			.create(
 				<BrowserRouter>
-					<Link to="/myreads/search">Add a book</Link>
+					<Link to="/myreads">Add a book</Link>
 				</BrowserRouter>
 			)
 			.toJSON()
