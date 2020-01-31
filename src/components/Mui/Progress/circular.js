b6df77610c0d539ec2c22c332cc0bdaaf3f00e86
Heidi Mason <hapaheidi@gmail.com>
2020-01-31 12:31:09 -0800

    style: Fix indentation

--- src/components/Mui/Progress/circular.js
+++ src/components/Mui/Progress/circular.js
@@ -3,21 +3,21 @@ import CircularProgress from 'material-ui/CircularProgress'
 import { black } from 'material-ui/styles/colors'
 
 const styles = {
-  circularProgress: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '80px'
-  }
+    circularProgress: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '80px'
+    }
 }
 
 const LoadingAnimation = () => (
 	<div style={styles.circularProgress}>
-  	<CircularProgress
+      	<CircularProgress
     		size={80}
     		thickness={5}
     		color={black}
-  	/>
+      	/>
 	</div>
 )
 
